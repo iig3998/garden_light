ba815850d033cf4fbffa7626b312e0a39167273d
Fabio Molon <fabiomolon85@gmail.com>
2025-07-17 18:55:46 +0200

    Add function for manage timer2

--- lib/timer/timer2.h
+++ lib/timer/timer2.h
@@ -3,5 +3,30 @@
 
 #pragma once
 
+#define MAJOR_TIMER2_VER 0
+#define MINOR_TIMER2_VER 1
+#define PATCH_TIMER2_VER 0
+
+#include <stdint.h>
+
+void timer2_init(uint8_t mode, uint8_t output, uint8_t prescaler);
+
+void timer2_set_counter(uint8_t counter);
+
+void timer2_enable_interrupt_overflow();
+
+void timer2_disable_interrupt_overflow();
+
+void timer2_enable_interrupt_compa();
+
+void timer2_enable_interrupt_compb();
+
+void timer2_enable_interrupt_compb();
+
+void timer2_disable_interrupt_compb();
+
+void timer2_set_output_compare_register_a(uint8_t match_value);
+
+void timer2_set_output_compare_register_b(uint8_t match_value);
 
 #endif
